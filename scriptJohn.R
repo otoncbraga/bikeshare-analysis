df8abf317af67acdf5d5cd9dfe6a3f401c233a18
johnattandouglas <johnattandouglas@gmail.com>
2018-10-30 14:11:43 -0300

    Inserção de coluna com o dia da semana

--- scriptJohn.R
+++ scriptJohn.R
@@ -12,6 +12,19 @@
 # Outono: 21 set - 20 dez
 # Inverno: 21 dez - 20 mar
 
+#Monday, January 2, 2017  New Year's Day**
+#Monday, January 16, 2017  Martin Luther King Jr. Day
+#Friday, January 20, 2017 Inauguration Day***
+#Monday, February 20, 2017 Washington's Birthday
+#Monday, April 17, 2017 DC Emancipation Day**
+#Monday, May 29, 2017 Memorial Day
+#Tuesday, July 4, 2017  Independence Day
+#Monday, September 4, 2017  Labor Day
+#Monday, October 9, 2017  Columbus Day
+#Friday, November 10, 2017  Veterans Day*
+#Thursday, November 23, 2017  Thanksgiving Day
+#Monday, December 25, 2017  Christmas Day
+
 ###############################
 ########## COMANDOS ###########
 ###############################
@@ -19,10 +32,20 @@
 #para formar dataFrame apartir de outro
 #(dados = data.frame(x,y,outraTabela))
 
+#mostra o tipo de objeto
+#class(nomeDoObjeto)
+
 #para renomear colunas
 #names(dados) = c("Variável 1", "Variável 2")
 
 #rbind() para juntar linhas / cbind() para juntar colunas / merge()
+
+#Formatar data para dia da semana
+#format.Date(as.Date(datas), "%a") # abreviado
+#format.Date(as.Date(datas), "%A") # nome do dia completo
+
+#duas funções acima são equivalentes às seguintes:
+#weekdays(as.Date(datas), abbreviate = TRUE) ou weekdays(as.Date(datas))
 
 ###############################
 ######### PENDÊNCIAS ##########
@@ -38,24 +61,64 @@
 ###############################
 
 #Instalar bibliotecas
-install.packages("ggplot2") #gerar gráficos
-install.packages("sqldf") #reconhecer comandos SQL
-install.packages("dplyr") #Transformação/manipulação de dados
+install.packages("ggplot2"); #gerar gráficos
 
+install.packages("sqldf"); #reconhecer comandos SQL
+install.packages("dplyr"); #Transformação/manipulação de dados
+install.packages("lubridate");
 #Carregar bibliotecas
-library("ggplot2")
-library("sqldf")
-library("dplyr")
+library("ggplot2");
+library("sqldf");
+library("dplyr");
+library("lubridate");
 
-Q1 <- read.csv("C:/Users/johnattan.douglas/Desktop/capitalbikeshare/2017Q1-capitalbikeshare-tripdata.csv")
-Q2 <- read.csv("C:/Users/johnattan.douglas/Desktop/capitalbikeshare/2017Q2-capitalbikeshare-tripdata.csv")
-Q3 <- read.csv("C:/Users/johnattan.douglas/Desktop/capitalbikeshare/2017Q3-capitalbikeshare-tripdata.csv")
-Q4 <- read.csv("C:/Users/johnattan.douglas/Desktop/capitalbikeshare/2017Q4-capitalbikeshare-tripdata.csv")
-
+Q1 <- read.csv("C:/Users/johnattan.douglas/Desktop/capitalbikeshare/2017Q1-capitalbikeshare-tripdata.csv");
+Q2 <- read.csv("C:/Users/johnattan.douglas/Desktop/capitalbikeshare/2017Q2-capitalbikeshare-tripdata.csv");
+Q3 <- read.csv("C:/Users/johnattan.douglas/Desktop/capitalbikeshare/2017Q3-capitalbikeshare-tripdata.csv");
+Q4 <- read.csv("C:/Users/johnattan.douglas/Desktop/capitalbikeshare/2017Q4-capitalbikeshare-tripdata.csv");
 
 #Juntar as 4 partes em um único lugar
-QTOTAL <- rbind(rbind(Q1, Q2), rbind(Q3, Q4))
+QTOTAL <- rbind(rbind(Q1, Q2), rbind(Q3, Q4));
 
-#exportarTabela  #!!!Ainda não está dando certo!!!!
-tabela <- data.frame(QTOTAL)
-write.csv(tabela, "teste.csv")
+#Adicionar coluna com dia do mês
+QTOTAL <- QTOTAL %>% mutate(dia = mday(Start.date))
+
+#Adicionar coluna com mês
+QTOTAL <- QTOTAL %>% mutate(mes = month(Start.date))
+
+#Adicionar coluna com ano
+QTOTAL <- QTOTAL %>% mutate(ano = year(Start.date))
+
+#Adicionar coluna com dia da semana (1 a 7)
+QTOTAL <- QTOTAL %>% mutate(weekday = wday(Start.date))
+
+#Adicionar coluna com dia da semana (nome)
+QTOTAL <- QTOTAL %>% mutate(weekday_name = wday(Start.date, label=TRUE, abbr = FALSE))
+
+
+
+#Adicionar estações do ano [[[AINDA NÃO ESTÁ FUNCIONANDO]]]
+#Filtra Estações
+#________________________
+# Primavera: 21 mar - 20 jun
+Primavera <- QTOTAL %>% filter(Start.date>=as.Date("2017-03-21") & Start.date<=as.Date("2017-06-20"))
+Primavera <- Primavera %>% mutate(season = "spring")
+#________________________
+# Verão: 21 jun - 20 set
+Verão <- QTOTAL %>% filter(Start.date>=as.Date("2017-06-21") & Start.date<=as.Date("2017-09-20"))
+Verão <- Verão %>% mutate(season = "summer")
+#________________________
+# Outono: 21 set - 20 dez
+Outono <- QTOTAL %>% filter(Start.date>=as.Date("2017-09-21") & Start.date<=as.Date("2017-12-20"))
+Outono <- Outono %>% mutate(season = "fall")
+#________________________
+# Inverno: 21 dez - 20 mar
+Inverno <- filter(QTOTAL, (Start.date>=as.Date("2017-12-21") & Start.date<=as.Date("2017-12-31")) & (Start.date>=as.Date("2017-01-01") & Start.date<=as.Date("2017-03-20")))
+Inverno <- Inverno %>% mutate(season = "winter")
+#Junta arquivos
+QTOTAL <- rbind(rbind(Primavera, Verão), rbind(Outono, Inverno));
+
+
+
+#exportar tabela para arquivo csv
+write.csv(QTOTAL, "tabela.csv")
